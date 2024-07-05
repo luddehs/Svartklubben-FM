617acbb9a0b89eb4b138ae9b77e0fc100ed727c5
Ludvig Hultgren Svensson <ludvig.hs@gmail.com>
2024-07-05 10:30:46 +0000

    Update project directory urlpatterns

--- my_project/urls.py
+++ my_project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from about import views as about_views
 
 urlpatterns = [
-    path('about/', about_views.about_me, name='about'),
+    path("about/", include("about.urls"), name="about-urls"),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
