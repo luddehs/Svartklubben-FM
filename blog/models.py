a82202b6321a29eae21fef78a004672da150df7f
Ludvig Hultgren Svensson <ludvig.hs@gmail.com>
2024-07-16 23:11:04 +0000

    Add label to blog models and index html

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    label = models.CharField(max_length=200, default='content_type')
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
