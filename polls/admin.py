6869f407dd7e4bc83bd2a3d05b8845b5ee964584
Ludvig Hultgren Svensson <ludvig.hs@gmail.com>
2024-07-17 08:18:29 +0000

    Add choice to question in admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1  # Number of extra empty choices to display
 
-from .models import Question
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
 
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
