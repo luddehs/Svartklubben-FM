d0bfbe52f5bae7cf285e99e8b3d5ff8b1ab33609
Ludvig Hultgren Svensson <ludvig.hs@gmail.com>
2024-10-09 15:02:54 +0000

    Fix multiple choice bug. Refactor vote and delete_vote views

--- polls/views.py
+++ polls/views.py
@@ -32,9 +32,9 @@ class IndexView(LoginRequiredMixin, generic.ListView):
 
 class DetailView(LoginRequiredMixin, generic.DetailView):
     """
-    Displays the details of a poll question related to :model:`Question`.
-    This view shows the question and its available choices for logged-in users,
-    excluding unpublished questions.
+    Displays the details of a poll question and the available choices.
+    Retrieves the current user's vote for the question if it exists,
+    and adds it to the context.
     """
     model = Question
     template_name = "polls/detail.html"
@@ -47,15 +47,11 @@ class DetailView(LoginRequiredMixin, generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(DetailView, self).get_context_data(**kwargs)
-        try:
-            context['vote'] = (
-                ChoiceVote.objects
-                .get(users__in=[self.request.user],
-                     choice__question=self.get_object())
-            )
-
-        except ChoiceVote.DoesNotExist:
-            pass
+        context['vote'] = (
+            ChoiceVote.objects
+            .filter(users__in=[self.request.user], choice__question=self.get_object())
+            .first()
+        )
         return context
 
 
@@ -73,10 +69,13 @@ def vote(request, question_id):
     """
     Processes a user's vote for a question related to :model:`polls.Question`
     and :model:`polls.ChoiceVote`.
-    This view handles the logic for voting, including adding or changing a vote
-    for a specific choice and providing appropriate feedback to the user.
-    It updates the vote count and either informs the user of a successful vote
-    or an already existing vote.
+    This view handles the logic for voting, including adding, changing, or
+    preventing a duplicate vote for a specific choice. It updates the vote
+    count for both the old and new choices when applicable, and provides
+    feedback to the user regarding the vote's status.
+
+    If the user selects the same choice they already voted for, an information
+    message is displayed without updating the vote.
 
     **Context**
     ``question``
@@ -85,118 +84,88 @@ def vote(request, question_id):
     ``selected_choice``
          The specific instance of :model:`polls.Choice`
          the user selected to vote for.
-    ``user_vote``
-         The :model:`polls.ChoiceVote` instance representing
-         the user's vote for the selected choice.
+    ``user_previous_vote``
+         The user's previous vote, if it exists, for the given question.
+
     **Template:**
     :template:`polls/detail.html`
     """
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
-        user_vote, created = (
-            ChoiceVote.objects
-            .get_or_create(choice=selected_choice)
-        )
+        user_previous_vote = ChoiceVote.objects.filter(
+            choice__question=question, users=request.user
+        ).first()
 
-        vote_changed = False
-
-        if request.user not in user_vote.users.all():
-            user_vote.users.add(request.user)
-            user_vote.save()
-            selected_choice.votes = F("votes") + 1
-            selected_choice.save()
-        else:
+        if user_previous_vote and user_previous_vote.choice == selected_choice:
             messages.info(request, "You have already voted for this choice.")
-        return HttpResponseRedirect(
-            reverse("polls:results", args=(question.id,))
-        )
+            return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
-        previous_votes = ChoiceVote.objects.filter(choice__question=question)
-        for vote in previous_votes:
-            if (vote.choice != selected_choice and
-                    request.user in vote.users.all()):
+        if user_previous_vote:
+            user_previous_vote.users.remove(request.user)
+            user_previous_vote.choice.votes = F('votes') - 1
+            user_previous_vote.choice.save()
+            user_previous_vote.save()
 
-                vote.users.remove(request.user)
-                vote.choice.votes -= 1
-                vote.save()
-                vote.choice.save()
-                vote_changed = True
-
-        if vote_changed:
-            messages.success(
-                request,
-                "Your vote has been updated to the new choice."
-            )
-
-        else:
-            messages.success(request, "Your vote was successfully submitted.")
-
-        url = reverse("polls:results", args=(question.id,))
-        return HttpResponseRedirect(url)
+        user_vote, created = ChoiceVote.objects.get_or_create(choice=selected_choice)
+        user_vote.users.add(request.user)
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        messages.success(request, "Your vote was successfully submitted.")
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
     except (KeyError, Choice.DoesNotExist):
         error_message = "You didn't select a valid choice. Please try again."
         messages.error(request, error_message)
 
-        return render(
-            request,
-            "polls/detail.html",
-            {
-                "question": question,
-                "error_message": "You didn't select a choice.",
-            },
-        )
+        return render(request, "polls/detail.html", {"question": question, "error_message": error_message})
 
 
 @login_required
 def delete_vote(request, question_id):
     """
-    Deletes a user's vote for a question related to :model:`polls.Question`
-    and :model:`polls.ChoiceVote`.
-    This view allows users to remove their vote for a particular
-    and updates the vote count for the associated choice.
-    If the user has not voted on the question, an error message is displayed.
+    Handles the deletion of a user's vote for a question related to
+    :model:`polls.Question` and :model:`polls.ChoiceVote`. This view allows
+    users to confirm and remove their vote for a particular choice, and updates
+    the vote count for the associated choice.
+    If the user has not voted on the question, an error message is displayed,
+    and they are redirected back to the question's detail view. If a vote is
+    found and the user confirms the deletion (via a POST request),
+    the vote is deleted.
 
     **Context**
     ``question``
          The instance of :model:`polls.Question` identified by `question_id`.
     ``vote``
-         The instance of :model:`polls.ChoiceVote`
-         representing the user's vote on the question.
-         If the user hasn't voted, this variable will not exist.
+         The instance of :model:`polls.ChoiceVote` representing the user's vote
+         on the question. If the user has voted, this is passed to the confirmation
+         template for display. If the user hasn't voted, this is `None`.
+
     **Template:**
-    :template:`polls/delete.html`
+    :template:`polls/delete.html` (GET request for confirmation)
     """
     question = get_object_or_404(Question, pk=question_id)
-    try:
-        vote = ChoiceVote.objects.get(
-            users__in=[request.user],
-            choice__question=question
-        )
+    votes = ChoiceVote.objects.filter(users=request.user, choice__question=question)
 
-        if request.POST:
-            vote.users.remove(request.user)
-            vote.choice.votes -= 1
-            vote.save()
-            vote.choice.save()
-            messages.success(
-                request,
-                "Your vote has been successfully deleted."
-            )
-
-            return HttpResponseRedirect(
-                reverse("polls:detail", args=(question.id,))
-            )
-    except ChoiceVote.DoesNotExist:
+    if not votes.exists():
         messages.error(request, "You have not voted on this question.")
+        return HttpResponseRedirect(reverse("polls:detail", args=(question.id,)))
+
+    if request.method == "POST":
+        for vote in votes:
+            vote.users.remove(request.user)
+            vote.choice.votes = F('votes') - 1
+            vote.choice.save()
+            vote.save()
+
+        messages.success(request, "Your vote has been successfully deleted.")
+        return HttpResponseRedirect(reverse("polls:detail", args=(question.id,)))
 
     return render(
         request,
         "polls/delete.html",
         {
             "question": question,
-            "vote": vote,
-            "error_message": "You didn't select a choice.",
+            "vote": votes.first(),
         },
     )
